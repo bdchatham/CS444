bc83bfac685f5ce9eb0d21e198bc2209aef0a666
Brandon Chatham <chathamb@os-class.engr.oregonstate.edu>
2017-04-27 21:16:45 -0700

    Completed debugging. Need to fix edge-case deadlock and then Concurrency2 is complete.

--- Concurrency2/Concurrency2.c
+++ Concurrency2/Concurrency2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlin.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <sys/queue.h>
 #include <string.h>
@@ -15,51 +15,55 @@ struct philosopher
 };
 
 //Philosopher eating cycle function.
-void *supper(struct* philosopher)
+void *supper(void * args)
 {
-	int reponse;
+	struct philosopher* philo = (struct philosopher*)args;
+	int response = 0;
+	int action = 0;
 	int tries = 2;
 	//Infinitely cycle between eating and waiting.
 	while(1)
 	{
 		//Philosopher waits.
 		action = 1 + rand() % 20;
-		printf("%s is thinking for %d seconds.", philosopher->name, action);
+		printf("%s is thinking for %d seconds.\n", philo->name, action);
 		sleep(action);
 		
 		while(1)
 		{
 			//Hang for the left fork.
-			response = pthread_mutex_lock(philosopher->leftFork);
+			response = pthread_mutex_lock(philo->leftFork);
 		
 			if(!response)//Checks if lock was successfully taken.
 			{
 				if(tries > 0)
 				{
-					response = pthread_mutex_trylock(philosopher->rightFork);
+					response = pthread_mutex_trylock(philo->rightFork);
 					tries--;
 				}
 				else
 				{
-					response = pthread_mutex_lock(philosopher->rightFork);
+					response = pthread_mutex_lock(philo->rightFork);
 				}
 					
 				if(!response)
 				{
 					action = 2 + rand() % 8;
-					printf("%s has grabbed two forks and is eatting for %d seconds.", philsopher->name, action);
+					printf("%s has grabbed two forks and is eatting for %d seconds.\n", philo->name, action);
 					sleep(action);
 
 					//return the forks.
-					pthread_mutex_unlock(philosopher->leftFork);	
-					pthread_mutex_unlock(philosopher->rightFork);
+					pthread_mutex_unlock(philo->leftFork);	
+					pthread_mutex_unlock(philo->rightFork);
 
-					printf("%s has returned the forks.", philosopher->name);
+					printf("%s has returned the forks.\n", philo->name);
+					tries = 2;
+					break;
 				}
 			}
 		}	
 	}
-
+	
 	return NULL;
 }
 
@@ -68,27 +72,27 @@ void setup()
 {
 	int i, response;
 	pthread_mutex_t forks[5];
-	struct philosophers[5];
+	struct philosopher philosophers[5];
 
 	//Initialize mutex's
 	for(i = 0; i < 5; i++)
 	{
-		response = pthread_mutex_init(forks[i], NULL);
+		response = pthread_mutex_init(&forks[i], NULL);
 		
 		//Check if failed to create mutex.
 		if(response)
 		{
-			printf("Failed to create mutex. Exitting....");
+			printf("Failed to create mutex. Exiting....");
 			exit(1);
 		}
 	}
 
 	//Initializing philosophers and setup threads
-	philosophers[0].name = "Abe";
-	philosophers[1].name = "Bob";
-	philosophers[2].name = "Carly";
-	philosophers[3].name = "Daruma";
-	philosophers[4].name = "Flume";
+	strcpy(philosophers[0].name, "Abe");
+	strcpy(philosophers[1].name,"Bob");
+	strcpy(philosophers[2].name, "Carly");
+	strcpy(philosophers[3].name, "Daruma");
+	strcpy(philosophers[4].name,"Flume");
 
 
 	for(i = 0; i < 5; i++)
@@ -96,18 +100,18 @@ void setup()
 		philosophers[i].leftFork = &forks[i];
 
 		//Modulus needed for last philosopher in the round robin who shares the 4th and 0th forkl indices. 
-		philosoperts[i].rightFork = &forks[(i + 1) % 5];
+		philosophers[i].rightFork = &forks[(i + 1) % 5];
 		pthread_attr_t attr;
 		pthread_attr_init (&attr);
-		threadReponse = pthread_create(&philosophers.threadHandle, &attr, supper, &philosophers[i]); 
+		philosophers[i].threadResponse = pthread_create(&philosophers[i].threadHandle, &attr, supper, &philosophers[i]); 
 	}
 
 	for(i = 0; i < 5; i++)
 	{
 		//Check if the thread creation and joining fail.
-		if(!philosopher[i].threadResponse && pthread_join(philosopher.threadHandle, NULL))
+		if(!philosophers[i].threadResponse && pthread_join(philosophers[i].threadHandle, NULL))
 		{
-			printf("Failed to join thread for %s. Exiting...", philosopher[i].name);
+			printf("Failed to join thread for %s. Exiting...", philosophers[i].name);
 			exit(1);
 		} 
 	}
